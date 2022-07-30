44d2698301c22bb4bc5a8d73cf6fe0472ef5d6c3
shivaay <42311911+HUNTER1622@users.noreply.github.com>
2022-07-30 13:48:01 +0530

    Add project files.

---
+++ BulkyBookWeb/Data/AppDBInitializer.cs
@@ -0,0 +1,39 @@
+﻿using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Data
+{
+    public class AppDBInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuider)
+        {
+            using (var serviceScope = applicationBuider.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<ApplicationDBContext>();
+                context.Database.EnsureCreated();
+
+                if (!context.Categories.Any())
+                {
+                    context.Categories.AddRange(new List<Category>()
+                    {
+                        new Category()
+                        {
+                            Name="Horror",
+                            DisplayOrder=0,
+                            CreatedDateTime=DateTime.Now,
+
+                        },
+                        new Category()
+                        {
+                            Name="Fiction",
+                            DisplayOrder=0,
+                            CreatedDateTime=DateTime.Now,
+                        }
+
+                    });
+                    context.SaveChanges();
+                }
+                
+            }
+        }
+    }
+}
