0d4288eb938e2d88cba895fe45c59eec09e2e254
shivaay <42311911+HUNTER1622@users.noreply.github.com>
2022-08-26 00:51:03 +0530

    Implement Area In project Create Different Areas for Admin and Customer

--- BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -3,14 +3,15 @@ using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Model;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyBookWeb.Controllers
+namespace BulkyBookWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork db;
         public CategoryController(IUnitOfWork _db)
         {
-           db = _db;
+            db = _db;
         }
 
         public IActionResult Index()
@@ -29,11 +30,11 @@ namespace BulkyBookWeb.Controllers
         {
             //Custom validation we can add through key value pairs
             //Server-Side Validation
-            if(model.Name == model.DisplayOrder.ToString())
+            if (model.Name == model.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("CustomError", "Both values are not same");
             }
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 db.Category.Add(model);
                 db.Save();
@@ -46,12 +47,12 @@ namespace BulkyBookWeb.Controllers
         //Edit Section
         public IActionResult Edit(int? id)
         {
-            if(id == null || id < 0)
+            if (id == null || id < 0)
             {
                 return NotFound();
             }
-            var CategoryDetail = db.Category.GetFirstOrDefault(x=> x.Id == id);
-            if(CategoryDetail == null)
+            var CategoryDetail = db.Category.GetFirstOrDefault(x => x.Id == id);
+            if (CategoryDetail == null)
             {
                 return NotFound();
             }
@@ -77,7 +78,7 @@ namespace BulkyBookWeb.Controllers
             {
                 return NotFound();
             }
-            var CategoryDetail = db.Category.GetFirstOrDefault(x=>x.Id==id);
+            var CategoryDetail = db.Category.GetFirstOrDefault(x => x.Id == id);
             return View(CategoryDetail);
         }
         [HttpPost]
---
+++ BulkyBookWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,66 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _db;
+        public CoverTypeController(IUnitOfWork db)
+        {
+            _db = db;  
+        }
+        public IActionResult Index()
+        {
+            var data = _db.CoverType.GetAll();
+            return View(data);
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]  
+        public IActionResult Add(CoverType model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.CoverType.Add(model);
+                _db.Save();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult GetDetailsById(int id)
+        {
+            var databyid = _db.CoverType.GetFirstOrDefault(x=>x.CoverTypeId == id); 
+            return View(databyid);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CoverType model)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.CoverType.Update(model);
+                _db.Save();
+            }
+           
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var datadeletebyid = _db.CoverType.GetFirstOrDefault(x=>x.CoverTypeId==id);
+            if(datadeletebyid!=null)
+            {
+                _db.CoverType.Remove(datadeletebyid);
+                _db.Save();
+            }
+               
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
